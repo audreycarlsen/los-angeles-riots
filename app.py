cc0980f188d4442f99c0e0293a0956c9c91952d0
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-27 14:46:52 -0700

    dried up app.py:

--- app.py
+++ app.py
@@ -6,20 +6,17 @@ from operator import itemgetter
 
 app = Flask(__name__)
 
+parsed_csv = csv.DictReader(open('./static/la-riots-deaths.csv'))
+csv_list = list(parsed_csv)
+for person in csv_list:
+  person['date'] = datetime.strptime(person['date'], '%Y-%m-%d')
+
 @app.route('/')
 def index():
-  parsed_csv = csv.DictReader(open('./static/la-riots-deaths.csv'))
-  csv_list = list(parsed_csv)
-  for person in csv_list:
-    person['date'] = datetime.strptime(person['date'], '%Y-%m-%d')
   return render_template('index.html', victims_list = csv_list)
 
 @app.route('/sorted/<attribute>')
 def sorted_table(attribute):
-  parsed_csv = csv.DictReader(open('./static/la-riots-deaths.csv'))
-  csv_list = list(parsed_csv)
-  for person in csv_list:
-    person['date'] = datetime.strptime(person['date'], '%Y-%m-%d')
   sorted_list = sorted(csv_list, key=itemgetter(attribute))
   return render_template('table.html', victims_list = sorted_list)
 
